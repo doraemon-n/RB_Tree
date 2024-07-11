3a4a187de0ef49355f36de61675ced6c79ff266e
热巧 <3147014964@qq.com>
2024-07-11 10:28:04 +0800

    Update RB_Tree_Node.cpp

--- RB_Tree_Node.cpp
+++ RB_Tree_Node.cpp
@@ -2,6 +2,8 @@
 #include "RB_Tree_Node.h"
 
 template <class T>
+// 采用模板类实现 增强通用性 
+// 定义节点类 包括左右孩子指针 父结点指针 颜色标签 数据
 RB_Tree_Node<T>::RB_Tree_Node(T data_in):data(data_in),color_tag(0),Left_child(NULL),Right_child(NULL),Father_Node(NULL)
 {
 	data = data_in;
